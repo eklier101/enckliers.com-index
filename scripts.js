c07f8e2367d5e02131b5cda187c1093fa6af6911
eklier101 <eklier101@gmail.com>
2025-04-22 13:35:34 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// This file can be used for future dynamic functionality if needed
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('Welcome to the Enckliers homepage!');
+});
